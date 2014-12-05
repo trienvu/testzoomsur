68f06e87ee1d0144f3256ec9f903d151834e6909
trienvu <trienvu135@gmail.com>
2014-12-05 14:17:05 +0700

    xx

---
+++ SurfaceZoom/src/com/example/surfacezoom/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.surfacezoom;
+
+import com.example.surfacezoom.PanAndZoomListener.Anchor;
+
+import android.app.Activity;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.FrameLayout;
+
+public class MainActivity extends Activity implements SurfaceHolder.Callback,
+		OnPreparedListener {
+	private SurfaceView mSurfaceView;
+	private SurfaceHolder mSurfaceHolder;
+	private static final String videoUrl = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
+	private MediaPlayer mMediaPlayer;
+	private FrameLayout mFrameLayout;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		mFrameLayout = (FrameLayout) this.findViewById(R.id.relMainView);
+		mSurfaceView = (SurfaceView) this.findViewById(R.id.surfaceview);
+		mSurfaceHolder = mSurfaceView.getHolder();
+		mSurfaceHolder.addCallback(this);
+
+		mSurfaceView.setOnTouchListener(new PanAndZoomListener(mFrameLayout,
+				mSurfaceView, Anchor.CENTER));
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		try {
+			mMediaPlayer = new MediaPlayer();
+			mMediaPlayer.setDisplay(mSurfaceHolder);
+			mMediaPlayer.setDataSource(videoUrl);
+			mMediaPlayer.prepare();
+			mMediaPlayer.setOnPreparedListener(this);
+			mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+
+	}
+
+	@Override
+	public void onPrepared(MediaPlayer mp) {
+		mMediaPlayer.start();
+	}
+
+}
---
+++ SurfaceZoom/src/com/example/surfacezoom/MyImageView.java
@@ -0,0 +1,5 @@
+package com.example.surfacezoom;
+
+public class MyImageView {
+
+}
